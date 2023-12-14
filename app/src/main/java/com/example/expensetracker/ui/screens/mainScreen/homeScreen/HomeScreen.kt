30b0e11a932cc06ebd7f8db6494a04b27899d5a0
Yash Goel <89704419+GoelYash1@users.noreply.github.com>
2023-12-14 20:32:52 +0530

    Minor changes in the home screen

--- app/src/main/java/com/example/expensetracker/ui/screens/mainScreen/homeScreen/HomeScreen.kt
+++ app/src/main/java/com/example/expensetracker/ui/screens/mainScreen/homeScreen/HomeScreen.kt
@@ -1,15 +1,117 @@
 package com.example.expensetracker.ui.screens.mainScreen.homeScreen
 
+import android.nfc.Tag
+import android.util.Log
+import android.widget.Toast
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyHorizontalGrid
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.expensetracker.data.entities.TransactionCategories
+import kotlin.random.Random
 
 @Composable
 fun HomeScreen() {
-    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center){
-        Text(text = "This is Home Screen")
+    Column(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Row(
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth(0.5f),
+                contentAlignment = Alignment.Center
+            ){
+                Button(
+                    onClick = { /*TODO*/ },
+                    modifier = Modifier.fillMaxWidth(),
+                    shape = RectangleShape
+                ) {
+                    Text(
+                        text = "Monthly",
+                        fontSize = 18.sp
+                    )
+                }
+            }
+            Spacer(modifier = Modifier.width(2.dp))
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                contentAlignment = Alignment.Center
+            ){
+                Button(
+                    onClick = { /*TODO*/ },
+                    modifier = Modifier.fillMaxWidth(),
+                    shape = RectangleShape
+                ) {
+                    Text(
+                        text = "Daily",
+                        fontSize = 18.sp
+                    )
+                }
+            }
+        }
+        Row {
+
+        }
+        LazyVerticalGrid(columns = GridCells.Fixed(2)){
+            items(TransactionCategories.categories){category->
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth(0.5f)
+                        .padding(2.dp)
+                        .background(Color.Cyan, RoundedCornerShape(25.dp))
+                        .clickable {
+
+                        }
+                ) {
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(16.dp),
+                        verticalAlignment = Alignment.CenterVertically,
+                        horizontalArrangement = Arrangement.SpaceBetween
+                    ) {
+                        Icon(
+                            painter = painterResource(id = category.iconResId),
+                            contentDescription = null
+                        )
+                        Text(
+                            text = category.name,
+                            fontWeight = FontWeight.ExtraBold,
+                            fontSize = 18.sp
+                        )
+                        Text(text = "0.0")
+                    }
+                }
+                Spacer(modifier = Modifier.padding(5.dp))
+            }
+        }
     }
 }
